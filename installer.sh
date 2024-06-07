cdd2deeb732621e64152781c0a90ed261affe5bd
Robert Zhang <louirobert@gmail.com>
2024-06-07 18:06:15 +0800

    Use nameref for function parameters

--- installer.sh
+++ installer.sh
@@ -2,15 +2,14 @@
 
 function show_help
 {
-  local target_dir=$1
-  shift 1
-  local files=("$@")
+  local -n target_dir_ref=$1
+  local -n files_ref=$2
   local name=${BASH_SOURCE[0]}
   echo "
 $name [-f] [-h]
 
-Install the following files to $target_dir:
-${files[*]}
+Install the following files to $target_dir_ref:
+${files_ref[*]}
 
 Options:
 -f: Remove existed file before installing a new one
@@ -20,13 +19,12 @@ Options:
 
 function test_files
 {
-  local target_dir=$1
-  shift 1
-  local files=("$@")
-  for f in "${files[@]}"; do
+  local -n target_dir_ref=$1
+  local -n files_ref=$2
+  for f in "${files_ref[@]}"; do
     # Test "-a" || "-L" for the case that a symbolic link exists but is invalid.
-    if [[ -a "$target_dir/$f" ]] || [[ -L "$target_dir/$f" ]]; then
-      echo "$target_dir/$f already exists! Remove it before installation or run with option '-f'."
+    if [[ -a "$target_dir_ref/$f" ]] || [[ -L "$target_dir_ref/$f" ]]; then
+      echo "$target_dir_ref/$f already exists! Remove it before installation or run with option '-f'."
       exit 1
     fi
   done
@@ -34,14 +32,13 @@ function test_files
 
 function remove_files
 {
-  local target_dir=$1
-  shift 1
-  local files=("$@")
-  for f in "${files[@]}"; do
+  local -n target_dir_ref=$1
+  local -n files_ref=$2
+  for f in "${files_ref[@]}"; do
     # Test "-a" || "-L" for the case that a symbolic link exists but is invalid.
-    if [[ -a "$target_dir/$f" ]] || [[ -L "$target_dir/$f" ]]; then
-      echo "Removing $target_dir/$f"
-      rm -rf "$target_dir/$f"
+    if [[ -a "$target_dir_ref/$f" ]] || [[ -L "$target_dir_ref/$f" ]]; then
+      echo "Removing $target_dir_ref/$f"
+      rm -rf "$target_dir_ref/$f"
     fi
   done
 }
